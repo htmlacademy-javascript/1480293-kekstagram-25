10771dc58f1771defb198203a27f9191051d1faf
Vitaliy Belykaov <66310947+Vitaliy-Bel@users.noreply.github.com>
2022-03-24 02:50:17 +0300

    Задание 4

--- js/main.js
+++ js/main.js
@@ -4,14 +4,9 @@ const getRandomtInclusive = function (min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   } else {
-
-    console.log('Ошибка: минимальное число больше максимального');
     return false;
   }
 };
-
-getRandomtInclusive (1,20);
-
 
 const getMaxLengthStr = function (str, maxlength) {
   if (str.length <= maxlength) {
@@ -19,4 +14,59 @@ const getMaxLengthStr = function (str, maxlength) {
   } return false;
 };
 
-getMaxLengthStr ('Надеюсь работает верно', 10);
+let id = 0;
+
+let url = '';
+
+const description = [
+  'Закат',
+  'Небо',
+  'Рассвет',
+  'Ночь',
+  'День',
+  'Красота',
+];
+
+const comments = [];
+
+let message = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const avatar = '';
+
+const name = [
+  'Артем',
+  'Петя',
+  'Вася',
+  'Света',
+];
+
+const createComments = () => {
+  return {
+    id: id++,
+    avatar: `img/avatar-${getRandomtInclusive(1,6)}.svg`,
+    message: message[getRandomtInclusive(0,message.length-1)],
+    name: name[getRandomtInclusive(0,name.length-1)],
+  };
+};
+
+const createPhotoDescription = () => {
+  return {
+    id: id++,
+    url: `photos/${id}.jpg`,
+    description: description[getRandomtInclusive(0,description.length-1)],
+    likes: getRandomtInclusive(15,200),
+    comments: Array.from({length: 3}, createComments),
+
+  };
+};
+
+const PhotoDescription = Array.from({length: 25}, createPhotoDescription);
+
+console.log(PhotoDescription);
