944577c89d250925ede83f1d568c407032e090d1
Vitaliy Belykaov <66310947+Vitaliy-Bel@users.noreply.github.com>
2022-04-13 23:59:35 +0300

    9-ое задание

--- js/user-modal.js
+++ js/user-modal.js
@@ -6,6 +6,7 @@ const userModalElement = document.querySelector('.img-upload__overlay');
 const userModalOpenElement = document.querySelector('#upload-file');
 const userModalCloseElement = document.querySelector('#upload-cancel');
 const image = document.querySelector('.img-upload__preview img');
+const effectsPreview = document.querySelector('.effects__preview');
 
 
 const onPopupEscKeydown = (evt) => {
@@ -33,6 +34,10 @@ function closeUserModal () {
 userModalOpenElement.addEventListener('change', function(evt) {
   evt.preventDefault();
   image.src = URL.createObjectURL(userModalOpenElement.files[0]);
+  /*effectsPreview.style.background-image = url('URL.createObjectURL(userModalOpenElement.files[0])');*/
+  /*effectsPreview.style.transform = 'scale(scaleValueImg)';
+
+  blob:http://localhost:3000/467be109-85bd-4c63-94b9-14af87102802*/
   openUserModal();
 });
 
@@ -100,9 +105,95 @@ fullPictureCloseElement.addEventListener('keydown', (evt) => {
   }
 });
 
-const form = document.querySelector('#upload-select-image');
+
+//SCALE
+const scaleSmaller = document.querySelector('.scale__control--smaller');
+const scaleBigger = document.querySelector('.scale__control--bigger');
+const scaleValue= document.querySelector('.scale__control--value');
+scaleSmaller.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  if (parseInt(scaleValue.value) >= 50) {
+
+    scaleValue.value = parseInt(scaleValue.value) - 25 + '%';
+    const scaleValueImg = parseInt(scaleValue.value)/100;
+    console.log(scaleValueImg)
+    image.style.transform = 'scale(scaleValueImg)';
+  }
+});
+
+scaleBigger.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  if (parseInt(scaleValue.value) <= 75) {
+    scaleValue.value = parseInt(scaleValue.value) + 25 + '%';
+    const scaleValueImg = parseInt(scaleValue.value)/100;
+    console.log(scaleValueImg)
+    image.style.transform = 'scale(scaleValueImg)';
+  }
+});
+
+//ФИЛЬТР
+
+/*const formImg = document.querySelector('img-upload__form');
+
+function onFilterChange (evt) {
+  if (evt.target.matches('.effects__preview')) {
+    console.log('evt.target.value');
+    console.log('evt.target.class');
+    document.querySelector('img-upload__preview').classList.add = evt.target.value;
+  }
+}
+
+formImg.addEventListener('click', onFilterChange);*/
+
+// Фильтр «Хром»
+
+const sliderElement = document.querySelector('.effect-level__slider');
+const valueElement = document.querySelector('.effect-level__value');
+
+
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 1,
+  },
+  start: 1,
+  step: 0.1,
+  connect: 'lower',
+});
+
+sliderElement.noUiSlider.on('update', () => {
+  valueElement.value = sliderElement.noUiSlider.get();
+});
+
+/*
+specialElement.addEventListener('change', (evt) => {
+  if (evt.target.checked) {
+    sliderElement.noUiSlider.updateOptions({
+      range: {
+        min: 1,
+        max: 10
+      },
+      start: 8,
+      step: 0.1
+});
+  } else {
+    sliderElement.noUiSlider.updateOptions({
+      range: {
+        min: 0,
+        max: 100,
+      },
+      step: 1
+    });
+    sliderElement.noUiSlider.set(80);
+  }
+});
+*/
 
 //Валидация
+const form = document.querySelector('#upload-select-image');
+
+
 const pristine = new Pristine(form, {
   classTo: 'img-upload__text',
   errorTextParent: 'img-upload__text',
